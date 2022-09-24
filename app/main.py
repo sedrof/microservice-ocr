e78ee364c1ba0d7e6fd90ace4381ed410f5bc00c
s3dr0f <abdalla_2030@hotmail.com>
2022-09-24 15:46:23 +0200

    fixes

--- app/main.py
+++ app/main.py
@@ -19,14 +19,14 @@ from pydantic import BaseSettings
 from PIL import Image
 
 class Settings(BaseSettings):
-    app_auth_token: str
+    app_auth_token: str = 'asdasd'
     debug: bool = False
     echo_active: bool = False
     app_auth_token_prod: str = None
     skip_auth: bool = False
 
-    class Config:
-        env_file = ".env"
+    # class Config:
+    #     env_file = ".env"
 
 @lru_cache
 def get_settings():
