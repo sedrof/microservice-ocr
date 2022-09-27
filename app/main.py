00a1d36be29be350dec9e0b358e2672ca5b0b244
s3dr0f <abdalla_2030@hotmail.com>
2022-09-27 14:50:06 +0200

    mac OS

--- app/main.py
+++ app/main.py
@@ -13,11 +13,16 @@ from fastapi import(
     UploadFile
     )
 import pytesseract
+import fitz
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseSettings
 from PIL import Image
+from . ocr import ocr_page
+from tempfile import NamedTemporaryFile
+import shutil
 
+pytesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\\tesseract.exe"
 class Settings(BaseSettings):
     app_auth_token: str = 'asdasd'
     debug: bool = False
@@ -25,8 +30,8 @@ class Settings(BaseSettings):
     app_auth_token_prod: str = None
     skip_auth: bool = False
 
-    # class Config:
-    #     env_file = ".env"
+    class Config:
+        env_file = ".env"
 
 @lru_cache
 def get_settings():
@@ -65,14 +70,58 @@ def verify_auth(authorization = Header(None), settings:Settings = Depends(get_se
 @app.post("/") # http POST
 async def prediction_view(file:UploadFile = File(...), authorization = Header(None), settings:Settings = Depends(get_settings)):
     verify_auth(authorization, settings)
-    bytes_str = io.BytesIO(await file.read())
+    print(file.filename, ' file......file')
     try:
-        img = Image.open(bytes_str)
+        suffix = pathlib.Path(file.filename).suffix
+        print(suffix, 'suffixxxx')
+        with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+            shutil.copyfileobj(file.file, tmp)
+            tmp_path = pathlib.Path(tmp.name)
+            print(tmp_path, 'tmp pathjjjjj')
+            doc = fitz.open(tmp_path)
+            mat = fitz.Matrix(400 / 72, 400 / 72)
+            img_filenames = []
+            i = 0
+            for page in doc:
+                pix = page.get_pixmap(matrix=mat)
+                img_filenames.append("page-%04i.png" % page.number) 
+                pix.pil_save(img_filenames[i], format="png", dpi=(300,300))
+                i+=1
+            print(img_filenames[1], 'pixxxx')
+            file.file.close()
+            tmp.close()
+            
     except:
-        raise HTTPException(detail="Invalid image", status_code=400)
-    preds = pytesseract.image_to_string(img)
-    predictions = [x for x in preds.split("\n")]
-    return {"results": predictions, "original": preds, 'tst':'tst'}
+        file.file.close()
+        tmp.close()
+        raise HTTPException(detail="Invalid file format", status_code=400)
+    try:
+        img = [Image.open(x) for y, x in enumerate(img_filenames)]
+    except:
+        raise HTTPException(detail="Invalid file", status_code=400)
+
+    # preds = [pytesseract.image_to_string(x) for y, x in enumerate(img)]
+    # predictions = [x for x in preds[0].split("\n")]
+    predictions = ocr_page(img[0])
+    file.file.close()
+    tmp.close()
+    print(tmp_path, 'in the last....')
+    shutil.rmtree(tmp_path)
+    return {"results": predictions, 'tst':'tst'}
+    # except:
+    #     raise HTTPException(detail="Invalid file before last", status_code=400)
+    
+
+    # bytes_str = io.BytesIO(await file.read())
+    # files = await file.read()
+    # print(type(files), ' typeeee')
+    # doc = fitz.open(files)
+    # page = doc.loadPage(0)
+    # pix = page.get_pixmap()
+    # output = "outfile.png"
+    # pix.save(output)
+    # bytes_str = io.BytesIO(pix.read())
+    
 
 
 @app.post("/img-echo/", response_class=FileResponse) # http POST
--- app/ocr.py
+++ app/ocr.py
@@ -1,15 +1,9 @@
-import pathlib
 import pytesseract
-from PIL import Image
 
-BASE_DIR = pathlib.Path(__file__).parent
-IMG_DIR = BASE_DIR / "images"
-img_path = IMG_DIR / "ingredients-1.png"
 
-img = Image.open(img_path)
+pytesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\\tesseract.exe"
 
-preds = pytesseract.image_to_string(img)
-predictions = [x for x in preds.split("\n")]
-# model.predict(img)
-
-print(predictions)
+def ocr_page(page: str):
+    preds = pytesseract.image_to_string(page)
+    predictions = [x for x in preds.split("\n")]
+    return predictions
